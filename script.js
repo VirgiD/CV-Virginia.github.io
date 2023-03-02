4e706866b38a963df99b378543069cff5684edab
VirgiD <virginiadanitz@gmail.com>
2023-03-02 20:46:28 -0300

    Subiendo proyecto

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+document.getElementById("botonA").onclick=
+    function cambiar(){
+       document.getElementById("imagenLaptop").src="imagenes/certificado1.jpg";
+         
+      };       
+document.getElementById("botonB").onclick=
+      function cambiar(){
+         document.getElementById("imagenLaptop").src="imagenes/certificado2.jpg";
+      };              
+document.getElementById("botonC").onclick=
+      function cambiar(){
+         document.getElementById("imagenLaptop").src="imagenes/certificado3.jpg";
+      };
+document.getElementById("botonD").onclick=
+      function cambiar(){
+         document.getElementById("imagenLaptop").src="imagenes/certificado4.jpg";
+      };      
+document.getElementById("boton").onclick=      
+    function restaurar() {
+    document.getElementById("imagenLaptop").src = "imagenes/laptop-working-in-living-room-at-home.jpg";
+  } ;
+
+
+
